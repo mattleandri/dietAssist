60b3f74b8dbc78e94090c9569310a25edf72cbf5
Mateo <mattleandri>
2023-11-09 15:53:52 -0300

    middleware verifyUsername added

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,15 @@
 import { Router } from "express";
-import logIn from "../controllers/auth.js";
+import {logIn, signUp} from "../controllers/auth.js";
+import { verifyUsername } from "../middlewares/auth.js";
 
 
 const authRouter=Router()
 
 authRouter.post('/login',logIn)
+authRouter.post('/signup',
+verifyUsername,
+signUp)
+
 
 export default authRouter
 
